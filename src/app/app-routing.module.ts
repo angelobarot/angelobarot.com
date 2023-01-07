6f9e960ee5a49f21db5899e25f3530f6b2d7d2d6
Angelo <jangelocastillobarot@gmail.com>
2023-01-08 00:11:35 +0800

    Create pages under construction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './modules/about/about.component';
+import { CareerComponent } from './modules/career/career.component'
+import { HomeComponent } from './modules/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, pathMatch: 'full'
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'career', component: CareerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'my-website'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-website');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-website app is running!');
-  });
 });
