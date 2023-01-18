8ff63756a0da36270d5a807565569f8120e7db11
Angelo <jangelocastillobarot@gmail.com>
2023-01-19 00:36:40 +0800

    Update website using reactjs instead of angular

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './assets/images/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="content">
+      <img className="logo" src={logo} alt="angelobarot.com" />
+      <h1>angelobarot</h1>
+      <div className="underconstruction">
+        <p>#underconstruction</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
